62b7e3b9eba96be9db828dc45fdff4d483e91574
Mark Dean <mpmdean@gmail.com>
2019-06-07 03:46:45 +0900

    Update example

--- make_test_files.py
+++ make_test_files.py
@@ -22,8 +22,12 @@ with h5py.File('test1.h5', 'w') as h5file:
     h5file.create_dataset('group1/data2', data=np.arange(4,10))
 
 with h5py.File('test2.h5', 'w') as h5file:
+    h5file.create_dataset('group2/data1', data=np.arange(3, 7))
+    h5file.create_dataset('group2/data2', data=np.arange(3, 7))
     h5file.create_dataset('group2/data3', data=np.arange(3, 7))
     h5file.create_dataset('group2/data4', data=np.arange(2,12))
+    h5file.create_dataset('group2/data5', data=np.arange(2,12))
+
 
 with h5py.File('linked_file.h5','w') as h5file:
     h5file.create_dataset('non_linked_data/data5', data=np.linspace(0, 4))
